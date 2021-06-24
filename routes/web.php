4116d8becf4f0d075d4105dde10e9670572fda42
月太げつたい <me@moontai0724.tw>
2021-06-25 01:33:11 +0800

    fix: remove default page

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::prefix('auth')->name('auth.')->group(function () {
     Route::get('login', [AuthController::class, 'login'])->name('login');
     Route::post('login', [AuthController::class, 'authenticate'])->name('login');
