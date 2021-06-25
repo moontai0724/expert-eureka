22512f344bea09fd0460dfc930e7c762ed41218d
月太げつたい <me@moontai0724.tw>
2021-06-25 16:26:02 +0800

    feat: title tooltip of post clickable

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -1,8 +1,10 @@
 <article class="post">
-    <aside class="clickable" onclick="javascript:location.href='{{ route('user', ['user' => $userId]) }}'">
+    <aside class="clickable" onclick="javascript:location.href='{{ route('user', ['user' => $userId]) }}'"
+           title="查看使用者文章">
         <img src="{{ asset("img/avatar.png") }}" alt="avatar">
     </aside>
-    <main @if($id != null) class="clickable" onclick="javascript:location.href='{{ route('detail', ['post' => $id]) }}'"@endif>
+    <main @if($id != null) title="查看文章與相關回應" class="clickable"
+          onclick="javascript:location.href='{{ route('detail', ['post' => $id]) }}'"@endif>
         @if($title != null)
             <h2 class="post-title">{{ $title }}</h2>
         @endif
